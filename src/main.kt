1b7f3b9d45eae8d6bab2f61a879467b65b6c2eba
camilo <cyateya35439@universidadean.edu.co>
2018-01-24 23:50:00 -0500

    nuevo codigo

--- src/main.kt
+++ src/main.kt
@@ -3,17 +3,6 @@
 fun main (args:Array<String>){
 
 
-    val  cantidades = 0
-
-   when(cantidades){
-       0 -> print("falta tocino")
-       100 -> print("tiene suficiente")//varios valores a la vez con la goma
-       else -> print("No hay cantidades")
-
-
-
-   }
-
 
 
 }
